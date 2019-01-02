91c6886e17407a17e26213a70ad6f3558b90480f
Goh Khee Teck <khee_teck_goh@apple.com>
2019-01-02 22:06:04 +0800

    alignment

--- requirements.R
+++ requirements.R
@@ -4,15 +4,15 @@
 
 source("https://raw.githubusercontent.com/portergoh/capstone/master/functions.R")
 
-packages<-c("rvest",
-            "fastDummies", 
-            "GGally", 
-            "tidyverse",
-            "httr",
-            "jsonlite",
-            "ggthemes",
-            "modelr",
-            "broom")
+packages <- c("rvest",
+              "fastDummies", 
+              "GGally", 
+              "tidyverse",
+              "httr",
+              "jsonlite",
+              "ggthemes",
+              "modelr",
+              "broom")
 #-----------------------------------------------------------------------#
 # Check if packages are missing from system and install them if 
 # necessary, otherwise load the require libraries
