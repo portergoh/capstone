088904f2e08cf6aa81e430f18c90e596479828bc
Goh Khee Teck <khee_teck_goh@apple.com>
2019-01-03 21:26:19 +0800

    first commit

---
+++ smur/R/smur_install.R
@@ -0,0 +1,21 @@
+#' Helper function to install the smur package
+#'
+smur_install <- function ()
+{
+  # Define some vars
+  smur_tgz  = "smur_0.1.0.tgz"
+  smur_from = "https://github.com/portergoh/capstone/raw/master/download/smur_0.1.0.tgz"
+  smur_to = file.path (getwd(), smur_tgz, fsep=.Platform$file.sep )
+
+  # Download source.
+  download.file (smur_from, smur_to, method="auto", mode="wb")
+
+  # Install source.
+  install.packages(smur_to, type="source", repos=NULL)
+
+  # Clean up
+  unlink (smur_to)
+
+  # Load the prerequisite libraries into R session
+  source("https://raw.githubusercontent.com/portergoh/capstone/master/smur/R/requirements.R")
+}
