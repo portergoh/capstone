31cfd7b935ff4294009e39ec728da78155ec06ca
Goh Khee Teck <khee_teck_goh@apple.com>
2019-01-03 21:06:48 +0800

    first commit

---
+++ smur/R/requirements.R
@@ -0,0 +1,17 @@
+#-----------------------------------------------------------------------#
+#' Load the required packages for smur
+#-----------------------------------------------------------------------#
+packages <- c("ggplot2",
+	            "rvest",
+              "fastDummies",
+              "GGally",
+              "tidyverse",
+              "httr",
+              "jsonlite",
+              "ggthemes",
+              "modelr",
+              "broom")
+
+source("functions.R")
+check_packages(packages)
+
